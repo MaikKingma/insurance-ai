260f5ce02f30e0f82803da3ea3cbc3ba675c88f5
Maik Kingma <maikkingma@kingmas.me>
2024-11-06 14:29:59 +0100

    fixed products

--- backend/src/main/java/ai/insurance/domain/product/Product.java
+++ backend/src/main/java/ai/insurance/domain/product/Product.java
@@ -36,17 +36,15 @@ public class Product extends PanacheEntityBase {
     @Column(name = "coverage_description", nullable = false)
     private String coverageDescription;
 
-//    @Column(name = "prices", nullable = false)
-//    @JdbcTypeCode(SqlTypes.JSON)
-//    private Set<Price> prices = new HashSet<>();
+    @Column(name = "prices", nullable = false)
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Set<Price> prices = new HashSet<>();
 
-//    @Column(name = "category", nullable = false)
-//    @Enumerated(EnumType.STRING)
-//    private InsuranceCategory category;
-//
-//    @Column(name = "coverage_category", nullable = false)
-//    @Enumerated(EnumType.STRING)
-//    private CoverageCategory coverageCategory;
+    @Column(name = "category", nullable = false)
+    private InsuranceCategory category;
+
+    @Column(name = "coverage_category", nullable = false)
+    private CoverageCategory coverageCategory;
 
     public static Product create(String name, String defaultDescription, String coverageDescription, Double monthlyPrice, Double yearlyPrice, InsuranceCategory category) {
         Product product = new Product();
@@ -54,8 +52,8 @@ public class Product extends PanacheEntityBase {
         product.name = name;
         product.defaultDescription = defaultDescription;
         product.coverageDescription = coverageDescription;
-//        product.prices = Set.of(new Price(monthlyPrice, PaymentCycle.MONTHLY), new Price(yearlyPrice, PaymentCycle.YEARLY));
-//        product.category = category;
+        product.prices = Set.of(new Price(monthlyPrice, PaymentCycle.MONTHLY), new Price(yearlyPrice, PaymentCycle.YEARLY));
+        product.category = category;
 
         return product;
     }
--- backend/src/main/java/ai/insurance/query/product/ProductQueries.java
+++ backend/src/main/java/ai/insurance/query/product/ProductQueries.java
@@ -3,10 +3,12 @@ package ai.insurance.query.product;
 import ai.insurance.domain.product.Product;
 import ai.insurance.domaininteraction.product.ProductService;
 import ai.insurance.query.product.api.ProductQueriesApi;
+import ai.insurance.query.product.model.PriceView;
 import ai.insurance.query.product.model.ProductView;
 import io.smallrye.mutiny.Uni;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProductQueries implements ProductQueriesApi {
 
@@ -29,6 +31,15 @@ public class ProductQueries implements ProductQueriesApi {
         productView.setId(product.getId().toString());
         productView.setName(product.getName());
         productView.setDescription(product.getDefaultDescription());
+        productView.setCoverageDescription(product.getCoverageDescription());
+        productView.setCategory(product.getCategory().name());
+        productView.setCoverageCategory(product.getCoverageCategory().name());
+        productView.setPrice(product.getPrices().stream().map(price -> {
+            PriceView priceView = new PriceView();
+            priceView.setAmount(price.amount());
+            priceView.setPaymentCycle(price.paymentCycle().name());
+            return priceView;
+        }).toList());
         return productView;
     }
 }
